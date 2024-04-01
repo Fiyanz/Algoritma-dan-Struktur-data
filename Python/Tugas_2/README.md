5e997b09f6d792360b8da92ed15fcd6e54a0a890
Fiyanz <dctbagalyus@gmail.com>
2024-04-02 05:33:42 +0700

    update readme, add case 2, add flowchart

--- Python/Tugas_2/README.md
+++ Python/Tugas_2/README.md
@@ -1,13 +1,46 @@
 # Tugas ke 2 Algoritma Pemrograman dan Basis Data
 
-Algoritma menghitung nilai rata rata mahasiswa (case 1)
+## Algoritma menghitung nilai rata rata mahasiswa (case 1)
+**Narative Deskriptive**
 1. Mulai/start
-2. Memasukan input NIM mahasiwa
+2. Memasukan NIM mahasiwa
 3. Memasukan nama mahasiswa
-4. Masukan input nilai tugas 1
-5. Masukan input nilai tugas 2
-6. Masukan input nilai tugas 3
+4. Memasukan nilai tugas 1
+5. Memasukan nilai tugas 2
+6. Memasukan nilai tugas 3
 7. Menghitung rata rata nilai mahasiswa
 8. Menampilkan nilai rata rata mahasiswa
 9. Program selesai
 
+### Flowchart menghitung nilai rata rata
+**Img Flowchart**
+link img
+
+
+### Pseudo Code menghitung rata rata
+```
+belum ada mah
+```
+
+
+## Algoritma menghitung nilai akhir
+**Narative Deskriptive**
+1. Mulai
+2. Memasukan nama mahasiswa
+3. Memasukan nilai murni keaktifan
+4. Memasukan nilai murni tugas
+5. Memasukan nilai murni ujian
+6. Proses menghitung nilai keaktifan, tugas, ujian
+7. Proses menghitung nilai akhir
+8. menampilkan nilai akhir
+9. Selesai
+
+### Flowchart menghitung nilai akhir
+**Img Flowchart**
+link img
+
+
+### Pseudo Code
+```
+menyusul
+```
---
+++ Python/Tugas_2/nilai_akhir.py
@@ -0,0 +1,33 @@
+def nilai(keaktifan: int | float, tugas: int | float, ujian: int | float) -> int | float:
+    """
+    nilai keaktifan * dengan 20%
+    nilai tugas * dengan 50%
+    nilai ujian * dengan 30%
+    """
+    nilai_keaktifan = keaktifan * (20/100)
+    nilai_tugas = tugas * (50/100)
+    nilai_ujian = ujian * (30/100)
+
+    return nilai_keaktifan, nilai_tugas, nilai_ujian
+
+
+
+nama = input("Nama Mahasiswa: ")
+keaktifan = float(input("Nilai Keaktifan: "))
+tugas = float(input("Nilai Tugas: "))
+ujian = float(input("Nilai Ujian: "))
+
+#menghitung nilai
+nilai_keaktifan, nilai_tugas, nilai_ujian = nilai(keaktifan=keaktifan, tugas=tugas, ujian=ujian)
+
+#menghitung nilai akhir
+nilai_akhir = nilai_keaktifan + nilai_tugas + nilai_ujian
+
+#menampilkan nilai akhir
+print("=============================================================================")
+print(f"Mahasiswa yang bernama {nama}")
+print(f"Dengan Nilai Persentasi Yang dihasilkan")
+print(f"Nilai Keaktifan * 20% : {nilai_keaktifan}")
+print(f"Nilai Tugas * 50% : {nilai_tugas}")
+print(f"Nilai Ujian * 30% : {nilai_ujian}")
+print(f"Jadi Siswa yang bernama {nama} memperoleh nilai akhir sebesar {nilai_akhir}")
