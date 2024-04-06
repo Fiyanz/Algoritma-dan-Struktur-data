783e2f43ecd5a04791a1aed90fcfffa65a917839
Fiyanz <dctbagalyus@gmail.com>
2024-04-07 00:02:55 +0700

    update code tugas 3 bagian menghitung nilai akhir

--- Python/Tugas_3/NilaiAkhir/main.py
+++ Python/Tugas_3/NilaiAkhir/main.py
@@ -1,11 +1,38 @@
-#fungsi untuk menghitung nilai murni
-def nilaiMurni(keaktifan, tugas, ujian) -> float:
-    """
-    fungsi menghitung nilai murni dari keaktifan yang dikali 20%
-    kemudian nilai tugas dikali dengan 30%, dan terakhir nilai ujian 
-    dikali dengan 50%
+"""
+create by Fiyanz
+"""
+nama = input("Nama Siswa: ")
+nilaiKeaktifan = float(input("Nilai Keaktifan: "))
+nilaiTugas = float(input("Nilai Tugas: "))
+nilaiUjian = float(input("Nilai Ujian: "))
 
-    keaktifan: int atau float
-    tugas: int atau float
-    ujian: int atau float
-    """
+#menghitung nilai murni
+nilaiKeaktifan = nilaiKeaktifan * 0.2
+nilaiTugas = nilaiTugas * 0.3
+nilaiUjian = nilaiUjian * 0.5
+
+#nilai akhir
+nilaiAkhir = nilaiKeaktifan + nilaiTugas + nilaiUjian
+
+#seleksi nilai
+def seleksiNilai(nilaiAkhir) -> str:
+    if nilaiAkhir > 80:
+        return 'A'
+    elif nilaiAkhir > 70:
+        return 'B'
+    elif nilaiAkhir > 56:
+        return 'C'
+    elif nilaiAkhir > 46:
+        return 'D'
+    else:
+        return 'E'
+    
+grade = seleksiNilai(nilaiAkhir=nilaiAkhir)
+
+#menampilkan nilai
+print(f"Siswa yang bernama {nama}")
+print(f"Dengan Nilai Persentasi Yang dihasilkan.")
+print(f"Nilai Keaktifan * 20% : {nilaiKeaktifan}")
+print(f"Nilai Tugas * 30% : {nilaiTugas}")
+print(f"Nilai Ujian * 50% : {nilaiUjian}")
+print(f"Jadi Siswa yang bernama {nama} memperoleh nilai akhir sebesar {nilaiAkhir} dengan grade {grade}")
